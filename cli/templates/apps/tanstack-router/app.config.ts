db36af6585b8443c05ed626f17eec6073fb7f698
tiesen243 <ttien56906@gmail.com>
2025-06-08 14:47:40 +0700

    feat(CLI): validate env in tanstack router

--- cli/templates/apps/tanstack-router/app.config.ts
+++ cli/templates/apps/tanstack-router/app.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from '@tanstack/react-start/config'
 import react from '@vitejs/plugin-react-swc'
+import { createJiti } from 'jiti'
 import tsConfigPaths from 'vite-tsconfig-paths'
+
+const jiti = createJiti(import.meta.url)
+await jiti.import('@{{ name }}/env')
 
 export default defineConfig({
   tsr: { appDirectory: 'src' },
   vite: { plugins: [react(), tsConfigPaths()] },
 }) as unknown
+
