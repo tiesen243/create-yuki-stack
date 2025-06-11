67d44c1265334dd9e327698738e65f2ab69459c6
tiesen243 <ttien56906@gmail.com>
2025-06-12 02:11:07 +0700

    chore: minor changes

--- cli/templates/apps/tanstack-start/vite.config.ts
+++ cli/templates/apps/tanstack-start/vite.config.ts
@@ -1,11 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { createJiti } from 'jiti'
 import { defineConfig, loadEnv } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
-
-const jiti = createJiti(import.meta.url)
-await jiti.import('@{{ name }}/env')
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
