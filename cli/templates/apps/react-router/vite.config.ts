43427f6ff51f3f2fbc29dacd0b2786ea768a678f
tiesen243 <ttien56906@gmail.com>
2025-06-11 15:59:57 +0700

    feat: add env

--- cli/templates/apps/react-router/vite.config.ts
+++ cli/templates/apps/react-router/vite.config.ts
@@ -1,9 +1,15 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  process.env = { ...process.env, ...env }
+
+  return {
+    define: { 'process.env': process.env },
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  }
 })
 
--- cli/templates/apps/tanstack-start/vite.config.ts
+++ cli/templates/apps/tanstack-start/vite.config.ts
@@ -1,13 +1,19 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { createJiti } from 'jiti'
+import { defineConfig, loadEnv } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 const jiti = createJiti(import.meta.url)
-await jiti.import('@{{ name }}/env')
+await jiti.import('@my-yuki-app/env')
 
-export default defineConfig({
-  plugins: [tanstackStart(), tailwindcss(), tsConfigPaths()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  process.env = { ...process.env, ...env }
+
+  return {
+    define: { 'process.env': process.env },
+    plugins: [tanstackStart(), tailwindcss(), tsConfigPaths()],
+  }
 })
 
