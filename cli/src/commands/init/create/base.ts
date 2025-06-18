e2799d4a3ce457f5500c0df435257470a9ab167a
tiesen243 <ttien56906@gmail.com>
2025-06-18 20:49:38 +0700

    chore: minor changes

--- cli/src/commands/init/create/base.ts
+++ cli/src/commands/init/create/base.ts
@@ -4,34 +4,28 @@ import type { ProjectOptions } from '@/commands/init/types'
 
 export async function addBase(opts: ProjectOptions) {
   const templatePath = new URL('../templates/', import.meta.url)
+  const copyOptions = { recursive: true, force: true }
 
   await Promise.all([
     fs.copyFile(new URL('_gitignore', templatePath), '.gitignore'),
     fs.copyFile(new URL('tsconfig.json', templatePath), 'tsconfig.json'),
     fs.copyFile(new URL('turbo.json', templatePath), 'turbo.json'),
+    fs.cp(new URL('turbo', templatePath), 'turbo', copyOptions),
+    fs.cp(new URL('tools', templatePath), 'tools', copyOptions),
+    fs.mkdir('apps', { recursive: true }),
+    fs.mkdir('packages', { recursive: true }),
     fs.writeFile('.env.example', '# Example environment variables\n'),
   ])
 
   await setupPackageManager(opts.packageManager, templatePath)
 
   await Promise.all([
-    fs.mkdir('apps', { recursive: true }),
-    fs.mkdir('packages', { recursive: true }),
-    fs.cp(new URL('tools', templatePath), 'tools', {
-      recursive: true,
-      force: true,
-    }),
-  ])
-
-  await Promise.all([
-    fs.cp(new URL('packages/validators', templatePath), 'packages/validators', {
-      recursive: true,
-      force: true,
-    }),
-    fs.cp(new URL('packages/ui', templatePath), 'packages/ui', {
-      recursive: true,
-      force: true,
-    }),
+    fs.cp(new URL('packages/ui', templatePath), 'packages/ui', copyOptions),
+    fs.cp(
+      new URL('packages/validators', templatePath),
+      'packages/validators',
+      copyOptions,
+    ),
   ])
 }
 
