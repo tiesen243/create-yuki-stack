e7c40702f4f7e359ddceebce8897c359f9d65af0
tiesen243 <ttien56906@gmail.com>
2025-11-07 10:06:26 +0700

    fix: execute script

--- cli/tsdown.config.ts
+++ cli/tsdown.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  shims: true,
+  dts: true,
   clean: true,
+  shims: true,
+  exports: true,
   outputOptions: {
     banner: '#!/usr/bin/env node\n',
   },
