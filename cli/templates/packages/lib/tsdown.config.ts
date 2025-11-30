e6eee153ee59c7e5e4110697c46cb08435203731
tiesen243 <ttien56906@gmail.com>
2025-11-30 10:59:34 +0700

    feat: update exports

--- cli/templates/packages/lib/tsdown.config.ts
+++ cli/templates/packages/lib/tsdown.config.ts
@@ -4,15 +4,5 @@ export default defineConfig({
   entry: ['./src/*.ts'],
   dts: true,
   shims: true,
-  exports: {
-    customExports() {
-      return {
-        './*': {
-          types: './dist/*.d.mts',
-          default: './dist/*.mjs',
-        },
-        './package.json': './package.json',
-      }
-    },
-  },
+  exports: true,
 })
