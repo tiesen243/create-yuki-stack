57d1d2ad9430808f21b5ea50d87f27d323410194
tiesen243 <ttien56906@gmail.com>
2025-12-26 18:29:42 +0700

    fix: tanstack start

--- cli/templates/apps/react-router/vite.config.ts
+++ cli/templates/apps/react-router/vite.config.ts
@@ -1,6 +1,6 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => ({
--- cli/templates/apps/tanstack-start/src/router.tsx
+++ cli/templates/apps/tanstack-start/src/router.tsx
@@ -1,20 +1,14 @@
 import '@{{ name }}/validators/env'
 
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { createRouter } from '@tanstack/react-router'
 
 import { routeTree } from './routeTree.gen'
 
-export function createRouter() {
-  const router = createTanStackRouter({
+export const getRouter = () => {
+  const router = createRouter({
     routeTree,
     scrollRestoration: true,
   })
 
   return router
-}
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof createRouter>
-  }
 }
